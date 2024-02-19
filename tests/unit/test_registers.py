8e581f2f5e844f0fb4fb1a401f04f00ca41a0240
Matthew Ballance <matt.ballance@gmail.com>
2024-02-18 18:23:51 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- tests/unit/test_registers.py
+++ tests/unit/test_registers.py
@@ -134,17 +134,16 @@ class TestRegisters(TestBase):
                 }
                 return 0xFFFF_FFFF_FFFF_FFFF;
             }
+
+            pure function bit[64] get_offset_of_instance_array(string name, int index) {
+                return 0xFFFF_FFFF_FFFF_FFFF;
+            }
         }
 
         component my_regs : reg_group_c {
             reg_blk             blks[4];
 
             pure function bit[64] get_offset_of_instance(string name) {
-                if (name == "r1") {
-                    return 0;
-                } else if (name == "r2") {
-                    return 4;
-                }
                 return 0xFFFF_FFFF_FFFF_FFFF;
             }
 
@@ -154,6 +153,8 @@ class TestRegisters(TestBase):
                 }
                 return 0xFFFF_FFFF_FFFF_FFFF;
             }
+            /*
+             */
         }
 
         component pss_top {
@@ -171,6 +172,8 @@ class TestRegisters(TestBase):
 
 //                print("init_down running");
                 xxx.set_handle(reg_region);
+                /*
+                 */
             }
 
             action Entry {
@@ -178,18 +181,21 @@ class TestRegisters(TestBase):
                     bit[32]     val;
 
                     comp.xxx.blks[0].r2.write_val(25);
+                    comp.xxx.blks[1].r2.write_val(25);
+                    /*
                     comp.xxx.blks[0].r1.write_val(20);
                     comp.xxx.blks[3].r2.write_val(25);
                     comp.xxx.blks[3].r1.write_val(20);
                     val = comp.xxx.blks[0].r1.read_val();
                     print("val: %d", val);
+                     */
                 }
 /*
  */
             }
         }
         """
-        self.enableDebug(False)
+        self.enableDebug(True)
         self.loadContent(content)
         out = io.StringIO()
         actor = zspy.Actor("pss_top", "pss_top::Entry")
